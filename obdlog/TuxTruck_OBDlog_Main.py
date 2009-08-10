6816cb8af1a42b1db7fcecdf9ec08384b9475693
Jason Antman <jason@jasonantman.com>
2009-08-10 21:53:38 +0000

    working...

--- obdlog/TuxTruck_OBDlog_Main.py
+++ obdlog/TuxTruck_OBDlog_Main.py
@@ -1,8 +1,36 @@
+# TuxTruck_OBDlog_Main.py
 #
-# TuxTruck obdlog application - main class
-# Time-stamp: "2009-08-10 16:57:02 jantman"
-# $LastChangedRevision$
-# $HeadURL$
+# Time-stamp: "2009-08-10 17:27:16 jantman"
+#
+# +----------------------------------------------------------------------+
+# | TuxTruck Project      http://tuxtruck.jasonantman.com                |
+# +----------------------------------------------------------------------+
+# | Copyright (c) 2009 Jason Antman.                                     |
+# |                                                                      |
+# | This program is free software; you can redistribute it and/or modify |
+# | it under the terms of the GNU General Public License as published by |
+# | the Free Software Foundation; either version 3 of the License, or    |
+# | (at your option) any later version.                                  |
+# |                                                                      |
+# | This program is distributed in the hope that it will be useful,      |
+# | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+# | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+# | GNU General Public License for more details.                         |
+# |                                                                      |
+# | You should have received a copy of the GNU General Public License    |
+# | along with this program; if not, write to:                           |
+# |                                                                      |
+# | Free Software Foundation, Inc.                                       |
+# | 59 Temple Place - Suite 330                                          |
+# | Boston, MA 02111-1307, USA.                                          |
+# +----------------------------------------------------------------------+
+# |Please use the above URL for bug reports and feature/support requests.|
+# +----------------------------------------------------------------------+
+# | Authors: Jason Antman <jason@jasonantman.com>                        |
+# +----------------------------------------------------------------------+
+# | $LastChangedRevision::                                             $ |
+# | $HeadURL::                                                         $ |
+# +----------------------------------------------------------------------+
 #
 
 import Queue, threading, datetime, os.path
--- obdlog/test.py
+++ obdlog/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+"""
 import threading
 import datetime
         
@@ -11,3 +12,46 @@ class ThreadClass(threading.Thread):
 for i in range(2):
     t = ThreadClass()
     t.start()
+"""
+
+import collections
+
+q = collections.deque(maxlen=3)
+print q
+
+q.appendleft("ONE")
+print q
+q.appendleft("TWO")
+print q
+q.appendleft("THREE")
+print q
+q.appendleft("FOUR")
+print q
+
+print q.popleft()
+print q
+print q.popleft()
+print q
+print q.popleft()
+print q
+
+print "======================\n"
+
+q = collections.deque(maxlen=3)
+print q
+
+q.append("ONE")
+print q
+q.append("TWO")
+print q
+q.append("THREE")
+print q
+q.append("FOUR")
+print q
+
+print q.pop()
+print q
+print q.pop()
+print q
+print q.pop()
+print q
